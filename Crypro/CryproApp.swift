364cbca45111041ccb04e6237fcf99d57c356866
Sergey Kravtsov <mr.kravtsov@bk.ru>
2023-04-11 22:07:45 +0800

    Initial Commit

---
+++ Crypro/CryproApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryproApp.swift
+//  Crypro
+//
+//  Created by Екатерина Долгина on 11.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct CryproApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
