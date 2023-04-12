8c1e0a8cd45190a4c6ba7ef5d129cf28693ecdf5
Sergey Kravtsov <mr.kravtsov@bk.ru>
2023-04-12 22:02:02 +0800

    added ViewModel

--- Crypro/Core/Components/CircleButton/CircleButtonAnimationView.swift
+++ Crypro/Core/Components/CircleButton/CircleButtonAnimationView.swift
@@ -16,7 +16,9 @@ struct CircleButtonAnimationView: View {
             .stroke(lineWidth: 5.0)
             .scale(animate ? 1.0 : 0.0)
             .opacity(animate ? 0.0 : 1.0)
-            .animation(animate ?  Animation.easeOut(duration: 1.0) : .none)
+            .onChange(of: animate) { _ in
+                withAnimation(Animation.easeOut(duration: 1.0)) {}
+            }
     }
 }
 
---
+++ Crypro/Core/Home/ViewModels/HomeViewModel.swift
@@ -0,0 +1,23 @@
+//
+//  HomeViewModel.swift
+//  Crypro
+//
+//  Created by Sergey Kravtsov on 12.04.2023.
+//
+
+import Foundation
+
+class HomeViewModel: ObservableObject {
+    
+    @Published var allCoins: [CoinModel] = []
+    @Published var portfolioCoins: [CoinModel] = []
+    
+    init() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+            self.allCoins.append(DeveloperPreview.instance.coin)
+            self.portfolioCoins.append(DeveloperPreview.instance.coin)
+
+        }
+    }
+
+}
